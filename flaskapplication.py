d60073bbb79f5eb19cf54ce527a0c84ab8135168
Emma Paris <naimul011@gmail.com>
2020-02-08 22:26:33 +0600

    A complete application

--- flaskapplication.py
+++ flaskapplication.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def index():
 	return '<h1>Hello World!</h1>'
 
+
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>Hello, %s!</h1>' % name
+
 if __name__ == '__main__':
 	app.run(debug=True)
