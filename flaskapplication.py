b910b5140b51b002fe6438aff27cc4c15ab72f3d
Emma Paris <naimul011@gmail.com>
2020-02-09 00:03:28 +0600

    Custom Error Pages

--- flaskapplication.py
+++ flaskapplication.py
@@ -4,6 +4,8 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+
+@app.route('/')
 @app.route('/index')
 def index():
 	return render_template('index.html')
@@ -11,6 +13,10 @@ def index():
 @app.route('/user/<name>')
 def user(name):
 	return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
 
 if __name__ == '__main__':
 	app.run(debug=True)
