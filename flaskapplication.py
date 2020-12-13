9ee8f9258f3cc6c3ef80acfe1b39cc8f89580f31
naimul011 <naimul011@gmail.com>
2020-12-13 23:34:59 +0600

    Flash Messaging - flaskapplication.py

--- flaskapplication.py
+++ flaskapplication.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
@@ -17,11 +17,16 @@ app.config['SECRET_KEY'] = '7cwtzhl0tkg9obj9'
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-	form = NameForm()
-	if form.validate_on_submit():
-		session['name'] = form.name.data
-		return redirect(url_for('index'))
-	return render_template('index.html', form=form, name=session.get('name'))
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html',
+        form = form, name = session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
